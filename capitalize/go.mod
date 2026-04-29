536a12a751366f1ebc1c1be9a49b913bbce644cd
chuokoh <chukwuebukajosh8@gmail.com>
2026-04-29 15:57:34 +0100

    string: capitalize

---
+++ capitalize/go.mod
@@ -0,0 +1,3 @@
+module capitalize
+
+go 1.26.1
---
+++ capitalize/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
+
+func main() {
+	fmt.Println(capitalize("éllo"))
+}
+
+func capitalize(s string) string {
+	split := strings.Fields(s)
+	r := []string{}
+
+	for _, c := range split {
+		r = append(r, cap(c))
+	}
+	return strings.Join(r, " ")
+}
+
+func cap(s string) string {
+	fChar := strings.ToUpper(string(s[0]))
+	rChar := strings.ToLower(s[1:])
+
+	return fChar + rChar
+}
+
+func capII(s string) string {
+	fChar := ""
+	rChar := ""
+
+	for _, c := range s {
+		fChar = strings.ToUpper(string(c))
+		break
+	}
+	rChar = strings.ToLower(s[1:])
+
+	return fChar + rChar
+}
+func capIII(s string)string{
+	fChar := ""
+	firstRuneSize := 0
+
+	for _, c := range s {
+		fChar = strings.ToUpper(string(c))
+		firstRuneSize = utf8.RuneLen(c)
+		break
+	}
+	rChar := strings.ToLower(s[firstRuneSize:])
+	return fChar + rChar
+}
